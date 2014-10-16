bbe81b25a7ba6c1a9c5e7282abe6d650b960afa8
jenali <e.v.yolkin@gmail.com>
2014-10-16 14:22:53 +0300

    simfony2/bundle

---
+++ simfony2/JobeetBundle/Controller/CategoryAdminController.php
@@ -0,0 +1,10 @@
+<?php
+#src/Ens/JobeetBundle/Controller/CategoryAdminController.php
+namespace Ens\JobeetBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class CategoryAdminController extends Controller
+{
+// Your code will be here
+}
---
+++ simfony2/JobeetBundle/Controller/JobAdminController.php
@@ -0,0 +1,10 @@
+<?php
+#src/Ens/JobeetBundle/Controller/JobAdminController.php
+namespace Ens\JobeetBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class JobAdminController extends Controller
+{
+    // Your code will be here
+}
